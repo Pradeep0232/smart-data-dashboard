54d0363971a5f430a8caa094fd84ca0ef66b268f
pradeep0232 <prad.kumar.v@gmail.com>
2025-07-03 13:10:16 +0530

    Allow all CORS origins for testing

--- backend/main.py
+++ backend/main.py
@@ -6,10 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://smart-data-dashboard.vercel.app"
-        ],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
